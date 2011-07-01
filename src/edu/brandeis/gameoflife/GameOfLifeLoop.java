d855c750064e7b2a598d3f77b8af7d5913cb65c2
Pito Salas <pitosalas@gmail.com>
2011-06-30 20:52:19 -0400

    new GameOfLifeLoop

---
+++ src/edu/brandeis/gameoflife/GameOfLifeLoop.java
@@ -0,0 +1,13 @@
+package edu.brandeis.gameoflife;
+
+import edu.brandeis.minigamee.GameController;
+import edu.brandeis.minigamee.GameLoop;
+import edu.brandeis.minigamee.GameModel;
+import edu.brandeis.minigamee.GameView;
+
+public class GameOfLifeLoop extends GameLoop {
+	public GameOfLifeLoop(GameView view, GameModel model, GameController controller) {
+		super(view, model, controller);
+	}
+
+}
